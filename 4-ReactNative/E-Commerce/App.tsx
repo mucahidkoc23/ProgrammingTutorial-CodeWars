5d2116be4ef9cb34c75c2948780843e4f720401d
mucahidkoc23 <mucahidkoc.tr@gmail.com>
2025-02-04 23:43:02 +0300

    organized

--- 4-ReactNative/E-Commerce/App.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Router from './src/pages/Router';
-
-const App = () => {  
-  return <Router/>
-}
-
-export default App;
--- 4-ReactNative/PersonelAssistant/App.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import Router from './src/pages/router';
-
-const App = () => {  
-  return <Router/>
-}
-
-export default App;
