6e9a98ef27db5b8206150403f64823f24365f32b
mucahidkoc23 <mucahidkoc.tr@gmail.com>
2025-02-05 22:53:03 +0300

    8kyu -TypeScript (First 20 popular solution)

--- KataSolution/TypeScript/8kyu/script.ts
+++ KataSolution/TypeScript/8kyu/script.ts
@@ -76,3 +76,98 @@
 // }
 
 // console.log(removeChar("Mücahid"));
+
+//---------------------------------------------String Repeat-----------------------------------------
+
+// function srtRepeat(n:number, str: string): string {
+//   return str.repeat(n);
+// }
+
+// console.log(srtRepeat(3, 'selim'));
+
+//----------------------------------------Grasshopper - Summation------------------------------------
+
+// function summation(n: number): number {
+//   let total = 0;
+//   for (let i = n; i >= 1; i--) {
+//     total += i;
+//   }
+//   return total;
+// }
+// console.log(summation(8));
+
+//--------------------------------Find the smallest integer in the array-----------------------------
+
+// function smallest(n: number[]): number {
+//   return Math.min(...n);
+// }
+
+// console.log(smallest([1, 2, 3]));
+
+//------------------------------------------Remove String Spaces-------------------------------------
+
+// function trim(str: string): string {
+//   return str.split(' ').join('');
+// }
+
+// console.log(trim(" 8 j 8 "));
+
+//-------------------------------------Basic Mathematical Operations---------------------------------
+
+// function basic(o:string, n1: number, n2: number) : number {
+//   return eval(n1 + o + n2 );
+// }
+
+// console.log(basic("+", 5,3));
+
+//---------------------------------------Abbreviate a Two Word Name----------------------------------
+
+// function firstSecond(f: string): string {
+//   let divide = f.split(" ");
+//   return divide[0].toLocaleUpperCase().slice(0, 1) + "." + divide[1].toLocaleUpperCase().slice(0, 1) + ".";
+// }
+
+// console.log(firstSecond("Mücahid kOÇ"));
+
+/*OR Solution 2
+
+function firstSecond(name: string): string {
+  return name.split(" ").map((item)=> item[0].toUpperCase()).join('.')
+}
+
+console.log(firstSecond("Mücahid KOÇ"));
+*/
+
+//-------------------------------------------Century From Year---------------------------------------
+
+// function calc(n: number): number {
+//   return Math.ceil(n / 100);
+// }
+
+// console.log(calc(1991));
+
+//--------------------------------------------Keep Hydrated!----------------------------------------
+
+// function calcH(n: number): number {
+//   return Math.trunc(n * 0.5);
+// }
+
+// console.log(calcH(11.8));
+
+//---------------------------------------A Needle in the Haystack-----------------------------------
+
+// function findN(str: any[]): string {
+//   let find = str.findIndex(x => x === 'needle').toString()
+//   return Number(find) < 0 ? 'Your function didn\'t return anything' : `found the needle at position ${find}`;
+// }
+
+// console.log(findN(['reg','needle','rebri',3,undefined]));
+
+//--------------------------------------Is n divisible by x and y?----------------------------------
+
+// function isDivisible(n: number, x: number, y: number): boolean {
+//   return n % x === 0 && n % y === 0 ? true : false;
+// }
+
+// console.log(isDivisible(12, 2, 5));
+
