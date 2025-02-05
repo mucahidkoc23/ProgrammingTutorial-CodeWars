5cbb2b03e8ac33c98bde8f20dcb8600efd1531dc
mucahidkoc23 <mucahidkoc.tr@gmail.com>
2025-02-05 20:28:54 +0300

    8kyu- TypeScript (First 10 popular solution)

---
+++ KataSolution/TypeScript/8kyu/script.js
@@ -0,0 +1,52 @@
+"use strict";
+//----------------------------------------------Even or Odd------------------------------------------
+// function evenOrOdd(n: number): string {
+//   return n % 2 === 0 ? "Even" : "Odd";
+// }
+// console.log(evenOrOdd(4));
+//-----------------------------------------------Multiply--------------------------------------------
+// function multiply(a: number, b: number): number {
+//   return a * b;
+// }
+// console.log(multiply(1, 1));
+//--------------------------------------------Return Negative----------------------------------------
+// function makeNegative(a: number): number {
+//   return a < 0 ? a : -a;
+// }
+// console.log(makeNegative(-5));
+//--------------------------------------------Sum of positive----------------------------------------
+// function positiveSum(arr: number[]): number {
+//   let arrP = arr.filter((x) => x > 0);
+//   return arrP.reduce((acc, crv) => acc + crv, 0);
+// }
+// console.log(positiveSum([5, -5, 1, 7]));
+//--------------------------------------------Reversed Strings---------------------------------------
+// function solution(a: string): string {
+//   return a.split('').reverse().join('');
+// }
+// console.log(solution("Mücahid"));
+//--------------------------------------Convert a Number to a String!--------------------------------
+// function numberToString(a: number): string {
+//   return a.toString();
+// }
+// console.log(numberToString(123));
+//-------------------------Convert boolean values to strings 'Yes' or 'No'.--------------------------
+// function boolToWord(a: boolean): string {
+//   return a === true ? 'Yes' : 'No';
+// }
+// console.log(boolToWord(true));
+//---------------------------------------------Square(n) Sum-----------------------------------------
+// function squareSum(numbers: number[]): number {
+//   return numbers.map((x) => x ** 2).reduce((acc, crv) => acc + crv, 0);
+// }
+// console.log(squareSum([1, 2, 2]));
+//--------------------------------------------Opposite number----------------------------------------
+// function opposite(a: number): number {
+//   return a < 0 ? -a : -a;
+// }
+// console.log(opposite(5));
+//------------------------------------Remove First and Last Character--------------------------------
+// function removeChar(a: string): string {
+//   return a.slice(1, -1);
+// }
+// console.log(removeChar("Mücahid"));
