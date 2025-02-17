1952c6ca0d105278bc53f55206383db6d0538eaa
mucahidkoc23 <mucahidkoc.tr@gmail.com>
2025-02-17 17:14:30 +0300

    7kyu - JavaScript (Solutions to 22 questions in popular questions with OOP)

--- CodeWars - KataSolution/JavaScript/7kyu/script.js
+++ CodeWars - KataSolution/JavaScript/7kyu/script.js
@@ -331,3 +331,70 @@ console.log(solution('abc', 'bc'));
 // }
 
 // console.log(findNextSquare(100));
+
+//---------------------------------------Animals and Inheritance------------------------------------
+
+// class Animal {
+//   constructor(name, age, legs, species, status) {
+//     this.name = name;
+//     this.age = age;
+//     this.legs = legs;
+//     this.species = species;
+//     this.status = status;
+//   }
+//   introduce() {
+//     return `Hello, my name is ${this.name} and I am ${this.age} years old.`;
+//   }
+// }
+
+// class Shark extends Animal {
+//   constructor(name, age, status, legs = 0, species = "shark") {
+//     super(name, age, legs, species, status);
+//   }
+// }
+
+// class Cat extends Animal {
+//   constructor(name, age, status, legs = 4, species = "cat") {
+//     super(name, age, legs, species, status);
+//   }
+//   introduce() {
+//     return `Hello, my name is ${this.name} and I am ${this.age} years old.  Meow meow!`;
+//   }
+// }
+
+// class Dog extends Animal {
+//   constructor(name, age, status, master, legs = 4, species = "dog") {
+//     super(name, age, legs, species, status);
+//     this.master = master;
+//   }
+//   greetMaster() {
+//     return `Hello ${this.master}`;
+//   }
+// }
+
+// const Animal1 = new Animal("Köpek Balığı", 18);
+
+// console.log(Animal1.introduce());
+
+// const dog = new Dog("Cathy", 7, "Playing with a ball of yarn", "Eliza");
+// console.log(dog.greetMaster());
+
+//----------------------------------------Super Class Extensions------------------------------------
+
+// class Animal {
+//   constructor(name) {
+//     this.name = name;
+//   }
+//   speak() {
+//     return this.name + " makes a noise, ";
+//   }
+// }
+
+// class Cat extends Animal {
+//   speak() {
+//     return super.speak() + this.name + ' goes meow.';
+//   }
+// }
+
+// let cat = new Cat("cat");
+// console.log(cat.speak());
