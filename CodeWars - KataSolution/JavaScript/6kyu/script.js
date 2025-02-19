4269bfcacaef90df2a1be45f9a389962c22b66a9
mucahidkoc23 <mucahidkoc.tr@gmail.com>
2025-02-19 22:33:39 +0300

    6kyu - JavaScript (Solutions to 3 questions in popular questions)

--- CodeWars - KataSolution/JavaScript/6kyu/script.js
+++ CodeWars - KataSolution/JavaScript/6kyu/script.js
@@ -17,7 +17,7 @@
 
 // console.log(solution(15));
 
-//---------------------------------------Stop gninnipS My sdroW!-----------------------------------
+//---------------------------------------Stop gninnipS My sdroW!------------------------------------
 
 // function spinWords(string) {
 //   return string
@@ -27,3 +27,21 @@
 // }
 
 // console.log(spinWords("This is a testxx"));
+
+//--------------------------------------------Who likes it?-----------------------------------------
+
+// function likes(names) {
+//   return names.length === 1
+//     ? `${names[0]} likes this`
+//     : names.length === 2
+//     ? `${names[0]} and ${names[1]} like this`
+//     : names.length === 3
+//     ? `${names[0]}, ${names[1]} and ${names[2]} like this`
+//     : names.length >= 4
+//     ? `${names[0]}, ${names[1]} and ${names.length - 2} others like this`
+//     : "no one likes this";
+// }
+
+// console.log(
+//   likes(["Jacob", "Alex", "Ale", "alis", "Jacob", "Alex", "Ale", "alis"])
+// );
